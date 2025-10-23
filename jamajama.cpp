82f1f90d2309b2ffc8e25b7f65c629d77cbc976d
unknown <kslearning101@gmail.com>
2025-10-23 09:35:40 +0500

    jama added

---
+++ jamajama.cpp
@@ -0,0 +1,8 @@
+#include <iostream>   // Header file for input/output stream
+
+using namespace std;  // Use the standard namespace
+
+int main() {
+    cout << "Hello, World!" << endl;  // Print output to console
+    return 0;  // Exit program successfully
+}
